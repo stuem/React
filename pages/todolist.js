d59362a48945dbaeeb3e041cf2fd496ed6afb11d
woaini101039 <1073547876@qq.com>
2020-11-24 09:01:58 +0800

    上传

--- pages/todolist.js
+++ pages/todolist.js
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 // import "./todolist.css"
 import {
-    ThemeProvider,
-    Editable, EditablePreview, EditableInput,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    FormControl, FormLabel,
     Input,
     Checkbox,
     Button,
-		Box,
+    Box,
 } from "@chakra-ui/core";
 function Index() {
-    const [count, setCount] = useState(""); 
-		const [list, setList] = useState([]);
-		const [checkedItems, setCheckedItems] = React.useState([false, true]);
-		console.log(checkedItems)
+    const [count, setCount] = useState("");
+    const [list, setList] = useState([]);
+    // const [checkedItems, setCheckedItems] = React.useState([false, true]);
+    // console.log(checkedItems)
     //增加页面内容
     const addData = (value) => {
         if (value) {
@@ -35,52 +24,52 @@ function Index() {
     //删除
     function liClick(e) {
         list.splice(e, 1);
-				setList([...list]);
+        setList([...list]);
     };
     //修改
-    function setCounTinp(e){
-        let input = document.getElementById("input"+e);
+    function setCounTinp(e) {
+        let input = document.getElementById("input" + e);
         var value = input.value;
         if (value) {
             list[e] = value;
             setList([...list]);
-            input.value="";
+            input.value = "";
         }
-			}
-			//完成
-			let isRed = false;
-			// setCheckedItems(e,index) {
-			// 	console.log(e,index);
-			// 	// if (isRed === true) {
-			// 	// 	isRed = false;
-			// 	// 	console.log(isRed);
-			// 	// }
-			// }
+    }
+    //完成
+    function setCheckedItems(e, i) {
+        let setche = document.getElementById("setche" + e);
+        if (i === true) {
+            setche.className = "done"
+        } else {
+            setche.className = ""
+        }
+    }
     return (
         <Box className="App" w="80%">
-				<Input errorBorderColor="red.300" value={count} onChange={(e) => setCount(e.target.value)} />
-				<Button variantColor="teal" size="xs" onClick={() => addData(count)}>添加</Button>
-				<h1><br></br></h1>
-				{
-						list.map((item, index) =>
-								<Box key={index}>
-										<Input id={"input"+index} placeholder={item} size="sm" w="20%" />
-										<Checkbox isChecked={checkedItems[1]} onChange={e => setCheckedItems([checkedItems, e.target.checked])} />
-										<span className={[isRed?"":"done"]}>{item}</span>
-										<Button h="1.75rem" size="sm" onClick={() => setCounTinp(index)}>修改</Button>
-										<Button h="1.75rem" size="sm" onClick={(e) => liClick(index)}>删除</Button>
-								</Box>
-						)
-				}
-				<style global jsx>
-					{`
+            <Input errorBorderColor="red.300" value={count} onChange={(e) => setCount(e.target.value)} />
+            <Button variantColor="teal" size="xs" onClick={() => addData(count)}>添加</Button>
+            <h1><br></br></h1>
+            {
+                list.map((item, index) =>
+                    <Box key={index}>
+                        <Input id={"input" + index} placeholder={item} size="sm" w="20%" />
+                        <Checkbox onChange={(e) => setCheckedItems(index, e.target.checked)} />
+                        <span id={"setche" + index}>{item}</span>
+                        <Button h="1.75rem" size="sm" onClick={() => setCounTinp(index)}>修改</Button>
+                        <Button h="1.75rem" size="sm" onClick={(e) => liClick(index)}>删除</Button>
+                    </Box>
+                )
+            }
+            <style global jsx>
+                {`
 							.done{
 								text-decoration: line-through;
 								color: #ccc;
 						}
 					`}
-					</style>
-      </Box>
+            </style>
+        </Box>
     );
 }
 export default Index
